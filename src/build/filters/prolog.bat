1bbb9ca98a338834ace6875a31514270c7bf0d49
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-01-06 10:42:12 -0400

    .bat script work fine but without args

--- src/build/filters/prolog.bat
+++ src/build/filters/prolog.bat
@@ -1,9 +1,8 @@
 @echo off
 
-@REM The script path to reference the included JRE java file
-SET SCRIPT_PATH=%~dp0
- 
-:APPEND_CLASSPATH
-SET CLASSPATH="%SCRIPT_PATH%\lib\*"
- 
-%SCRIPT_PATH%\jre\bin\java -classpath %CLASSPATH% ${Main.Class} %*
+SET CURRENT_DIRECTORY=%~dp0
+for %%B in (%CURRENT_DIRECTORY%.) do set PROLOBJECTLINK_HOME=%%~dpB
+SET CLASSPATH=%PROLOBJECTLINK_HOME%lib\*
+
+: default jdk
+java -classpath %CLASSPATH% ${Main.Class} %*
