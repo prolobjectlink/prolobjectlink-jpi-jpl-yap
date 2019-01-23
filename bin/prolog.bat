733ab85771ac3e121b9ea4ea7b885fc9a419d2f0
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-01-23 11:38:38 -0400

    Change prolog.[sh|bat] to pllink.[sh|bat]

--- bin/prolog.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-SET CURRENT_DIRECTORY=%~dp0
-for %%B in (%CURRENT_DIRECTORY%.) do set PROLOBJECTLINK_HOME=%%~dpB
-SET CLASSPATH=%PROLOBJECTLINK_HOME%lib\*
-
-: default jdk
-java -classpath %CLASSPATH% org.logicware.prolog.jpl.yap.YapPrologConsole %*
