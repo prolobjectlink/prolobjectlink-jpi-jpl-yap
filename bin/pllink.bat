7732f267ae8f92e65037cfd19339e8022ca84971
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-01-26 16:47:41 -0400

    Binary folder OK

---
+++ bin/pllink.bat
@@ -0,0 +1,8 @@
+@echo off
+
+SET CURRENT_DIRECTORY=%~dp0
+for %%B in (%CURRENT_DIRECTORY%.) do set PROLOBJECTLINK_HOME=%%~dpB
+SET CLASSPATH=%PROLOBJECTLINK_HOME%lib\*
+
+: default jdk
+java -classpath %CLASSPATH% org.logicware.prolog.jpl.yap.YapPrologConsole %*
