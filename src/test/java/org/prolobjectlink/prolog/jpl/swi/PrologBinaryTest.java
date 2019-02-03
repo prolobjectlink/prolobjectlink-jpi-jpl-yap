37ec67181f0cd4e67529cd2efb100c45f3d90665
Jose Zalacain <Jose Zalacain@10.11.0.30>
2019-02-03 13:49:20 -0400

    Introduce binary test

---
+++ src/test/java/org/prolobjectlink/prolog/jpl/swi/PrologBinaryTest.java
@@ -0,0 +1,102 @@
+package org.prolobjectlink.prolog.jpl.swi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.prolobjectlink.logging.LoggerConstants.FILE_NOT_FOUND;
+import static org.prolobjectlink.logging.LoggerConstants.IO;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.prolobjectlink.logging.LoggerUtils;
+import org.prolobjectlink.prolog.jpl.yap.YapProlog;
+
+public class PrologBinaryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test() throws FileNotFoundException, IOException {
+
+		// files existence
+		File bat = new File("bin/pllink.bat");
+		File sh = new File("bin/pllink.sh");
+		File src = new File("src/build/filters/bin.properties");
+		assertTrue(bat.exists());
+		assertTrue(sh.exists());
+		assertTrue(src.exists());
+
+		// build properties test
+		Properties bin = new Properties();
+		bin.load(new FileReader(src));
+		String script = bin.getProperty("Main.FileName");
+		assertEquals("pllink", script);
+		String main = bin.getProperty("Main.Class");
+		assertEquals(YapProlog.class.getName(), main);
+
+		//
+		String line = null;
+		StringBuilder b = null;
+		FileReader reader = null;
+		BufferedReader buffer = null;
+
+		try {
+			reader = new FileReader(bat);
+			buffer = new BufferedReader(reader);
+			line = buffer.readLine();
+			b = new StringBuilder();
+			while (line != null) {
+				b.append(line);
+				line = buffer.readLine();
+			}
+
+			assertTrue(b.toString().contains(YapProlog.class.getName()));
+
+			reader = new FileReader(sh);
+			buffer = new BufferedReader(reader);
+			line = buffer.readLine();
+			b = new StringBuilder();
+			while (line != null) {
+				b.append(line);
+				line = buffer.readLine();
+			}
+
+			assertTrue(b.toString().contains(YapProlog.class.getName()));
+
+		} catch (FileNotFoundException e) {
+			LoggerUtils.error(getClass(), FILE_NOT_FOUND, e);
+		} catch (IOException e) {
+			LoggerUtils.error(getClass(), IO, e);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					LoggerUtils.error(getClass(), IO, e);
+				}
+			}
+			if (buffer != null) {
+				try {
+					buffer.close();
+				} catch (IOException e) {
+					LoggerUtils.error(getClass(), IO, e);
+				}
+			}
+		}
+
+	}
+
+}
