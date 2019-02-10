4d80137cf3404860460522b7021cd92c23b8b3d6
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-02-10 13:07:33 -0400

    Shell script linux OK

--- bin/pllink.sh
+++ bin/pllink.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/bash
-SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
-if [ -e "/usr/bin/cygpath" ]
-then
-  LIB_PATH="$(cygpath -w $SCRIPT_PATH/lib/)*"
-else
-  LIB_PATH="$SCRIPT_PATH/lib/*"
-fi
-$SCRIPT_PATH/jre/bin/java -classpath "$LIB_PATH" org.prolobjectlink.prolog.jpl.yap.YapPrologConsole ${1+"$@"}
+java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.prolog.jpl.yap.YapPrologConsole ${1+"$@"}
--- src/build/filters/pllink.sh
+++ src/build/filters/pllink.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/bash
-SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
-if [ -e "/usr/bin/cygpath" ]
-then
-  LIB_PATH="$(cygpath -w $SCRIPT_PATH/lib/)*"
-else
-  LIB_PATH="$SCRIPT_PATH/lib/*"
-fi
-$SCRIPT_PATH/jre/bin/java -classpath "$LIB_PATH" ${Main.Class} ${1+"$@"}
+java -classpath "$(dirname "$(pwd)")/lib/*" ${Main.Class} ${1+"$@"}
