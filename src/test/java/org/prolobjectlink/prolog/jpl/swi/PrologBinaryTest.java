065107f2a172876ad0f0d025ee99eca663453192
Jose Zalacain <Jose Zalacain@AVALON>
2019-02-17 14:33:19 -0400

    Move down logger classes

--- src/test/java/org/prolobjectlink/prolog/jpl/swi/PrologBinaryTest.java
+++ src/test/java/org/prolobjectlink/prolog/jpl/swi/PrologBinaryTest.java
@@ -2,8 +2,8 @@ package org.prolobjectlink.prolog.jpl.swi;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.prolobjectlink.logging.LoggerConstants.FILE_NOT_FOUND;
-import static org.prolobjectlink.logging.LoggerConstants.IO;
+import static org.prolobjectlink.prolog.LoggerConstants.FILE_NOT_FOUND;
+import static org.prolobjectlink.prolog.LoggerConstants.IO;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -15,10 +15,9 @@ import java.util.Properties;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.prolobjectlink.logging.LoggerUtils;
 import org.prolobjectlink.prolog.jpl.yap.YapProlog;
 
-public class PrologBinaryTest {
+public class PrologBinaryTest extends PrologBaseTest {
 
 	@Before
 	public void setUp() throws Exception {
@@ -77,22 +76,22 @@ public class PrologBinaryTest {
 			assertTrue(b.toString().contains(YapProlog.class.getName()));
 
 		} catch (FileNotFoundException e) {
-			LoggerUtils.error(getClass(), FILE_NOT_FOUND, e);
+			provider.getLogger().error(getClass(), FILE_NOT_FOUND, e);
 		} catch (IOException e) {
-			LoggerUtils.error(getClass(), IO, e);
+			provider.getLogger().error(getClass(), IO, e);
 		} finally {
 			if (reader != null) {
 				try {
 					reader.close();
 				} catch (IOException e) {
-					LoggerUtils.error(getClass(), IO, e);
+					provider.getLogger().error(getClass(), IO, e);
 				}
 			}
 			if (buffer != null) {
 				try {
 					buffer.close();
 				} catch (IOException e) {
-					LoggerUtils.error(getClass(), IO, e);
+					provider.getLogger().error(getClass(), IO, e);
 				}
 			}
 		}
