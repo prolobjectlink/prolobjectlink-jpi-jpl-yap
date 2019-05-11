bff6eca7037e282330b72a9bb70a656e9152a97e
Jose Zalacain <Jose Zalacain@10.11.0.30>
2019-05-11 19:03:30 -0400

    Assembly OK

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-SET CURRENT_DIRECTORY=%~dp0
-SET CLASSPATH=%CURRENT_DIRECTORY%lib\*
-
-: default jdk
-java -classpath %CLASSPATH% org.prolobjectlink.prolog.jpl.yap.YapPrologConsole -g
--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-java -classpath "$(pwd)/lib/*" org.prolobjectlink.prolog.jpl.yap.YapPrologConsole -g
