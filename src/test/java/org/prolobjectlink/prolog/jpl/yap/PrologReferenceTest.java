75870ac66b586bf5d22886c713607fc55c0459bd
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-09-20 06:24:25 -0400

    Update pom.xml

---
+++ src/test/java/org/prolobjectlink/prolog/jpl/yap/PrologReferenceTest.java
@@ -0,0 +1,54 @@
+/*-
+ * #%L
+ * prolobjectlink-jpi-jpl-yap
+ * %%
+ * Copyright (C) 2012 - 2019 Prolobjectlink Project
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+package org.prolobjectlink.prolog.jpl.yap;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import javax.swing.JFrame;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.prolobjectlink.prolog.PrologEngine;
+import org.prolobjectlink.prolog.PrologQuery;
+
+public class PrologReferenceTest extends PrologBaseTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		PrologEngine engine = provider.newEngine();
+		PrologQuery query = engine.query("jpl_new( 'javax.swing.JFrame', ['frame with dialog'], F)");
+		Map<String, Object> m = query.oneVariablesResult();
+		assertEquals(JFrame.class, m.get("F").getClass());
+	}
+
+}
