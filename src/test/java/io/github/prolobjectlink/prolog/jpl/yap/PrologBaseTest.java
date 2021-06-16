3ee85b784d08e5e219633afadee015b140d05774
jzalacainllanes <jzalacainllanes@gmail.com>
2021-06-16 08:31:50 -0400

    Introduce get instance in prolog bootstrap class

--- src/test/java/io/github/prolobjectlink/prolog/jpl/yap/PrologBaseTest.java
+++ src/test/java/io/github/prolobjectlink/prolog/jpl/yap/PrologBaseTest.java
@@ -34,7 +34,6 @@ import io.github.prolobjectlink.prolog.PrologInteger;
 import io.github.prolobjectlink.prolog.PrologProvider;
 import io.github.prolobjectlink.prolog.PrologTerm;
 import io.github.prolobjectlink.prolog.PrologVariable;
-import io.github.prolobjectlink.prolog.jpl.yap.YapProlog;
 
 /** @author Jose Zalacain @since 1.0 */
 public abstract class PrologBaseTest {
@@ -53,7 +52,7 @@ public abstract class PrologBaseTest {
 	protected String employee = "employee";
 	protected String department = "department";
 
-	protected static final PrologProvider provider = Prolog.getProvider(YapProlog.class);
+	protected static final PrologProvider provider = Prolog.getInstance();
 	protected static final ScriptEngineManager manager = new ScriptEngineManager();
 
 	protected PrologTerm nil = provider.prologNil();
